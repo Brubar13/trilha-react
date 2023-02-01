dd802909b7391f1a39cda2496b95bba8e3820408
Bruno Barbella <brunobarbella@gmail.com>
2023-02-01 12:47:08 -0300

    Alterações fundamentos react

--- 01-fundamentos-reactjs/src/components/Post/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post/Post.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 import { Avatar } from "../Avatar/Avatar"
 import { Comment } from "../Comment/Comment"
 import styles from "./Post.module.css"
 
 export function Post({ author, content }){
+
+    const [comments, setComments] = useState([
+        1,
+        2
+    ])
+
+    function handleCrateNewComment() {
+          
+        setComments([...comments, comments.length + 1]);
+    }
+    
+
     return (
         <article className={ styles.post }>
             <header>
@@ -20,7 +33,7 @@ export function Post({ author, content }){
                 <p> { content }</p>
             </div>
 
-            <form className={ styles.commentForm }>
+            <form onSubmit={handleCrateNewComment()} className={ styles.commentForm }>
                 <strong>Deixe seu feedback</strong>
                 <textarea  placeholder="Deixe seu comentario"/>
 
@@ -31,8 +44,11 @@ export function Post({ author, content }){
             </form>
 
             <div className={ styles.commentList }>
-                <Comment image="https://xesque.rocketseat.dev/users/avatar/profile-5dc907af-84c2-45c3-a96f-42afb8165112-1622418282074.jpg" author="Bruno Barbella" commentContent="Esse é bão" />
-                <Comment image="https://pps.whatsapp.net/v/t61.24694-24/56770858_320622228646986_5172432146819710976_n.jpg?ccb=11-4&oh=01_AdTccp-20Xp3ktvGyw2fX3pM44cDShdJMDrsAty-JHwSCA&oe=63DD24D6" author="Leo Milos" commentContent="Atacado" />
+                {
+                    comments.map(comment => {
+                        return <Comment author="Bruno Barbella" image="https://xesque.rocketseat.dev/users/avatar/profile-5dc907af-84c2-45c3-a96f-42afb8165112-1622418282074.jpg" commentContent="É isso ai"  />
+                    })
+                }
             </div>
         </article>
     )
